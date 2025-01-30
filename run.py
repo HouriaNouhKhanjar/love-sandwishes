fa744f7592cdc71f6f03ff3008c72dfb55005f02
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-01-30 22:11:44 +0100

    Get the last five market values for each sandwish

--- run.py
+++ run.py
@@ -71,9 +71,25 @@ def calculate_surplus_data(sales_row):
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
     
-    surplus_data = [int(stock) - sales for stock, sales in zip(stock_row, sales_row)]
+    surplus_data = [int(stock) - sales for stock, sales in zip(stock_row, 
+                                                               sales_row)]
 
     return surplus_data
+
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
